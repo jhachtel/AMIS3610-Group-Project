b3f41da2e214fec1764cd74bafaeacd745b785c0
Kane Vastano <kmvastano@gmail.com>
2019-04-11 13:27:16 -0400

    Models 1.0 added

---
+++ api/Models/Trail.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace AMIS3610.GroupProject.Api.Models
+{
+    public class Trail
+    {
+        public int Id { get; set;}
+
+        public string Name {get; set;}
+
+        public Place place{get; set;}
+    }
+}
