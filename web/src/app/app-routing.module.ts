d652b3582920976bec0f69d9879c2abd2f315f4f
Jeff Hachtel <hachtel.5@osu.edu>
2019-04-13 19:30:42 -0400

    Modified the code to match Demerti fork

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { GearComponent }       from './gear/gear/gear.component';
+import { BooksComponent }       from './books/books/books.component';
 import { HomeComponent }        from './home/home.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'gear', component: GearComponent }
+  { path: 'books', component: BooksComponent }
 ];
 
 @NgModule({
