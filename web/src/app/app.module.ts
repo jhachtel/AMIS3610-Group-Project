690405e35368f72429b98e72550e3ac3f71adfd3
Jeff Hachtel <hachtel.5@osu.edu>
2019-04-14 13:59:47 -0400

    minor changes made, major changes needed.

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -5,14 +5,19 @@ import { HttpClientModule }   from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+//import { HomeComponent } from './home/home.component';
 import { LocationsComponent } from './locations/locations/locations.component';
-import { ActivitiesComponent } from './activities/activities/activities.component';
+//import { ActivitiesComponent } from './activities/activities/activities.component';
 import { GearComponent } from './gear/gear/gear.component';
 import { PeopleComponent } from './people/people/people.component';
 import { PlacesComponent } from './places/places/places.component';
 import { TrailsComponent } from './trails/trails/trails.component';
 import { TripsComponent } from './trips/trips/trips.component';
+// import { DashboardComponent } from ;
+// import { GearDetailComponent} from ;
+// import { MessagesComponent } from ;
+// import { GearSearchComponent } from ;
+// import { NavbarComponent } from ;
 
 @NgModule({
   imports: [
@@ -20,6 +25,12 @@ import { TripsComponent } from './trips/trips/trips.component';
     FormsModule,
     AppRoutingModule,
     HttpClientModule
+    //DashboardComponent,
+    //GearComponent,
+    //GearDetailComponent,
+    //MessagesComponent,
+    //GearSearchComponent,
+    //NavbarComponent
 
     // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
     // and returns simulated server responses.
@@ -30,25 +41,18 @@ import { TripsComponent } from './trips/trips/trips.component';
   ],
   declarations: [
     AppComponent,
-    HomeComponent,
+    //HomeComponent,
     LocationsComponent,
-    ActivitiesComponent,
+    //ActivitiesComponent,
     GearComponent,
     PeopleComponent,
     PlacesComponent,
     TrailsComponent,
     TripsComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-    DashboardComponent,
-    GearComponent,
-    GearDetailComponent,
-    MessagesComponent,
-    GearSearchComponent,
-    NavbarComponent
-  ],
   bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+
+export class AppModule {
+
+}
