a5fed85f2b7d3140a2bde0b9011920147b1030f4
Alex Tetzloff <alextetz@gmail.com>
2019-04-14 14:33:29 -0400

    fixed compiling issues

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,22 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-<<<<<<< HEAD
-import { HomeComponent } from './home/home.component';
-import { TripsComponent } from './trips/trips/trips.component';
-import { TrailsComponent } from './trails/trails/trails.component';
-import { GearComponent } from './gear/gear/gear.component';
-import { PeopleComponent } from './people/people/people.component';
-import { PlacesComponent } from './places/places/places.component';
-
-const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent},
-  { path: 'gear', component: GearComponent },
-  { path: 'people', component: PeopleComponent},
-  { path: 'places', component: PlacesComponent},
-  { path: 'trails', component: TrailsComponent},
-  { path: 'trips', component: TripsComponent}
-=======
 import { GearComponent }      from './gear/gear.component';
 import { DashboardComponent }   from './dashboard/dashboard.component';
 import { GearDetailComponent }  from './gear-detail/gear-detail.component';
@@ -26,7 +9,6 @@ const routes: Routes = [
   { path: 'gear', component: GearComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'detail/:id', component: GearDetailComponent }
->>>>>>> 788af3af0e31c255cf396ec4df604cfd2f8aa66c
 ];
 
 @NgModule({
--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,41 +1,27 @@
-<<<<<<< HEAD
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-=======
 import { NgModule }           from '@angular/core';
 import { BrowserModule }      from '@angular/platform-browser';
 import { FormsModule }        from '@angular/forms';
 import { HttpClientModule }   from '@angular/common/http';
->>>>>>> 788af3af0e31c255cf396ec4df604cfd2f8aa66c
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+
 import { GearComponent } from './gear/gear/gear.component';
 import { PeopleComponent } from './people/people/people.component';
 import { PlacesComponent } from './places/places/places.component';
 import { TrailsComponent } from './trails/trails/trails.component';
 import { TripsComponent } from './trips/trips/trips.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { GearDetailComponent } from './gear-detail/gear-detail.component';
+import { GearSearchComponent } from './gear-search/gear-search.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    FormsModule,
-    AppRoutingModule,
-    HttpClientModule
-
-    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
-    // and returns simulated server responses.
-    // Remove it when a real server is ready to receive requests.
-    // HttpClientInMemoryWebApiModule.forRoot(
-    //   InMemoryDataService, { dataEncapsulation: false }
-    // )
-  ],
+  
   declarations: [
     AppComponent,
-    HomeComponent,
+
     GearComponent,
     PeopleComponent,
     PlacesComponent,
@@ -45,17 +31,17 @@ import { NavbarComponent } from './navbar/navbar.component';
   ],
   imports: [
     BrowserModule,
-<<<<<<< HEAD
-    AppRoutingModule, HttpClientModule
-=======
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserModule,
+    AppRoutingModule,
     DashboardComponent,
     GearComponent,
     GearDetailComponent,
     MessagesComponent,
     GearSearchComponent,
     NavbarComponent
->>>>>>> 788af3af0e31c255cf396ec4df604cfd2f8aa66c
   ],
   bootstrap: [ AppComponent ]
 })
