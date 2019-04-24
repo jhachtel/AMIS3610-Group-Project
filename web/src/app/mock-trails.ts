20446986db2290c990da866107a053aee3d26c84
Jeff Hachtel <hachtel.5@osu.edu>
2019-04-24 04:17:25 -0400

    Fleshed out mock objects to match new definitions.

--- web/src/app/mock-trails.ts
+++ web/src/app/mock-trails.ts
@@ -1,14 +1,14 @@
 import { Trail } from './trail';
 
 export const TRAILS: Trail[] = [
-    { id: 11, name: 'Trail 11' },
-    { id: 12, name: 'Trail 12' },
-    { id: 13, name: 'Trail 13' },
-    { id: 14, name: 'Trail 14' },
-    { id: 15, name: 'Trail 15' },
-    { id: 16, name: 'Trail 16' },
-    { id: 17, name: 'Trail 17' },
-    { id: 18, name: 'Trail 18' },
-    { id: 19, name: 'Trail 19' },
-    { id: 20, name: 'Trail 20' }
+    { id: 11, name: 'Trail 11', description: 'Test Description', link: 'https://www.google.com/', places: undefined },
+    { id: 12, name: 'Trail 12', description: 'Test Description', link: 'https://www.google.com/', places: undefined },
+    { id: 13, name: 'Trail 13', description: 'Test Description', link: 'https://www.google.com/', places: undefined },
+    { id: 14, name: 'Trail 14', description: 'Test Description', link: 'https://www.google.com/', places: undefined },
+    { id: 15, name: 'Trail 15', description: 'Test Description', link: 'https://www.google.com/', places: undefined },
+    { id: 16, name: 'Trail 16', description: 'Test Description', link: 'https://www.google.com/', places: undefined },
+    { id: 17, name: 'Trail 17', description: 'Test Description', link: 'https://www.google.com/', places: undefined },
+    { id: 18, name: 'Trail 18', description: 'Test Description', link: 'https://www.google.com/', places: undefined },
+    { id: 19, name: 'Trail 19', description: 'Test Description', link: 'https://www.google.com/', places: undefined },
+    { id: 20, name: 'Trail 20', description: 'Test Description', link: 'https://www.google.com/', places: undefined }
 ];
--- web/src/app/mock-trips.ts
+++ web/src/app/mock-trips.ts
@@ -1,14 +1,14 @@
 import { Trip } from './trip';
 
 export const TRIPS: Trip[] = [
-    { id: 11, name: 'Trip 11' },
-    { id: 12, name: 'Trip 12' },
-    { id: 13, name: 'Trip 13' },
-    { id: 14, name: 'Trip 14' },
-    { id: 15, name: 'Trip 15' },
-    { id: 16, name: 'Trip 16' },
-    { id: 17, name: 'Trip 17' },
-    { id: 18, name: 'Trip 18' },
-    { id: 19, name: 'Trip 19' },
-    { id: 20, name: 'Trip 20' }
+    { id: 11, name: 'Trip 11', description: 'Test Description', companions: undefined, itemList: undefined, places: undefined, trails: undefined },
+    { id: 12, name: 'Trip 12', description: 'Test Description', companions: undefined, itemList: undefined, places: undefined, trails: undefined },
+    { id: 13, name: 'Trip 13', description: 'Test Description', companions: undefined, itemList: undefined, places: undefined, trails: undefined },
+    { id: 14, name: 'Trip 14', description: 'Test Description', companions: undefined, itemList: undefined, places: undefined, trails: undefined },
+    { id: 15, name: 'Trip 15', description: 'Test Description', companions: undefined, itemList: undefined, places: undefined, trails: undefined },
+    { id: 16, name: 'Trip 16', description: 'Test Description', companions: undefined, itemList: undefined, places: undefined, trails: undefined },
+    { id: 17, name: 'Trip 17', description: 'Test Description', companions: undefined, itemList: undefined, places: undefined, trails: undefined },
+    { id: 18, name: 'Trip 18', description: 'Test Description', companions: undefined, itemList: undefined, places: undefined, trails: undefined },
+    { id: 19, name: 'Trip 19', description: 'Test Description', companions: undefined, itemList: undefined, places: undefined, trails: undefined },
+    { id: 20, name: 'Trip 20', description: 'Test Description', companions: undefined, itemList: undefined, places: undefined, trails: undefined }
 ];
