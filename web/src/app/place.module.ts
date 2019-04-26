eb5c58672113e0f2387c6cb9086fa6c2084ca444
Jeff Hachtel <hachtel.5@osu.edu>
2019-04-26 12:44:10 -0400

    more angular mods

--- web/src/app/place.module.ts
+++ web/src/app/place.module.ts
@@ -2,13 +2,15 @@ import { NgModule }           from '@angular/core';
 import { PlacesComponent } from './places/places.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { PlaceService } from './place.service';
+
 @NgModule({
     imports: [
         AppRoutingModule
     ],
     exports: [PlacesComponent],
     declarations: [PlacesComponent],
-    providers: [],
+    providers: [PlaceService],
  })
  
  export class PlaceModule {
